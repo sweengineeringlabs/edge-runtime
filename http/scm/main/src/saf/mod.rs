c59bbabb5f83fc48f34fd078418628d931f2dfdc
Senior Agentic Engine Engineer <senior-agentic-engine@llmboot.local>
2026-06-22 20:15:38 +0200

    style: format runtime/http saf module imports

--- http/scm/main/src/saf/mod.rs
+++ http/scm/main/src/saf/mod.rs
@@ -6,9 +6,8 @@ mod validator_svc;
 
 pub use http::HttpIngress;
 pub use server_svc::HTTP_SERVER_SVC;
-pub use validator_svc::Validator;
 pub use swe_edge_ingress_http::{
-    FormPart, HttpAuth, HttpBody, HttpDecodeFn, HttpEncodeFn, HttpHealthCheck,
-    HttpIngressError, HttpIngressResult, HttpMethod, HttpRequest, HttpRequestBuilder,
-    HttpResponse,
+    FormPart, HttpAuth, HttpBody, HttpDecodeFn, HttpEncodeFn, HttpHealthCheck, HttpIngressError,
+    HttpIngressResult, HttpMethod, HttpRequest, HttpRequestBuilder, HttpResponse,
 };
+pub use validator_svc::Validator;
